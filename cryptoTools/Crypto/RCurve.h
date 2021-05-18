b127e307871028c5361a71eac87a6de50ef83381
YKLee-cn <yankaili2006@gmail.com>
2021-05-18 17:44:57 +0800

    Update RCurve.h

--- cryptoTools/Crypto/RCurve.h
+++ cryptoTools/Crypto/RCurve.h
@@ -6,8 +6,8 @@
 
 
 extern "C" {
-    #include "relic/relic_bn.h"
-    #include "relic/relic_ep.h"
+    #include "relic/include/relic_bn.h"
+    #include "relic/include/relic_ep.h"
 }
 #ifdef MONTY
 #undef MONTY
